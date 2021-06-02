98de2c5abcf3e7b74ace88de13a8c2c42270d3d7
unknown <shaikhjamilalrazi@gmail.com>
2021-06-03 01:48:40 +0600

    Development part

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Hello Jamil</Text>
+            <Text>Hello Jamil Development</Text>
             <StatusBar style="auto" />
         </View>
     );
