1e2901c0e3674d9008309c99dd70dbc9a0a2b127
unknown <shaikhjamilalrazi@gmail.com>
2021-06-03 11:49:52 +0600

    Task done by jamil

---
+++ components/auth/Register.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { View, Button, TextInput } from "react-native";
+import firebase from "firebase";
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+        };
+        this.onSignUp = this.onSignUp.bind(this);
+    }
+    onSignUp() {
+        const { name, email, password } = this.state;
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then((result) => {
+                firebase
+                    .firestore()
+                    .collection("users")
+                    .doc(firebase.auth().currentUser.uid)
+                    .set({
+                        name,
+                        email,
+                    });
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="name"
+                    onChangeText={(name) => this.setState({ name })}
+                />
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
+                <Button onPress={() => this.onSignUp()} title="Sign Up" />
+            </View>
+        );
+    }
+}
+
+export default Register;
