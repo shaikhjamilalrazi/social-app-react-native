d30c7befe045d1674fba30eb546a86ff9e4c4462
unknown <shaikhjamilalrazi@gmail.com>
2021-06-03 13:44:19 +0600

    added user search

--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
+import firebase from "firebase";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchUser, fetchUserPosts } from "../redux/actions/index";
 
 import FeedScreen from "./main/Feed";
 import ProfileScreen from "./main/Profile";
+import SearchScreen from "./main/Search";
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -37,6 +38,20 @@ export class Main extends Component {
                     }}
                 />
                 <Tab.Screen
+                    name="Search"
+                    component={SearchScreen}
+                    navigation={this.props.navigation}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="magnify"
+                                color={color}
+                                size={26}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
                     name="AddContainer"
                     component={EmptyScreen}
                     listeners={({ navigation }) => ({
@@ -58,6 +73,14 @@ export class Main extends Component {
                 <Tab.Screen
                     name="Profile"
                     component={ProfileScreen}
+                    listeners={({ navigation }) => ({
+                        tabPress: (event) => {
+                            event.preventDefault();
+                            navigation.navigate("Profile", {
+                                uid: firebase.auth().currentUser.uid,
+                            });
+                        },
+                    })}
                     options={{
                         tabBarIcon: ({ color, size }) => (
                             <MaterialCommunityIcons
